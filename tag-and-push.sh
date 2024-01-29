147ac08f248d470f5179ee4c3bbc10c3a20a5979
Arlo O'Keeffe <tiger@k5d.de>
2024-01-29 12:33:14 +0100

    initial commit

---
+++ tag-and-push.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+set -o errexit
+set -o nounset
+set -o xtrace
+
+MAJOR_MINOR=$(date -u +"%y%m")
+git fetch --prune --prune-tags --tags --force
+
+for MICRO in $(seq 100 999); do
+    VERSION=${MAJOR_MINOR}.0.${MICRO}
+    if git tag "v${VERSION}" && git push origin "v${VERSION}"; then
+        break
+    fi
+done
+
+echo "version=${VERSION}" >> "$GITHUB_OUTPUT"
