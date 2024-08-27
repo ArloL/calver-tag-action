0cb21be18f70efe41e0fb3ded20532b5622b38a7
Arlo O'Keeffe <digiput@k5d.de>
2024-08-27 19:57:21 +0200

    prevent release of 999

--- tag-and-push.sh
+++ tag-and-push.sh
@@ -8,6 +8,10 @@ MAJOR_MINOR=$(date -u +"%y%m")
 git fetch origin --prune 'refs/tags/*:refs/tags/*'
 
 for MICRO in $(seq 101 999); do
+    if [ "${MICRO}" = "999" ]; then
+        echo "Reached the last possible release. Something is off"
+        exit 1
+    fi
     VERSION=${MAJOR_MINOR}.0.${MICRO}
     if git tag "v${VERSION}" "${GITHUB_REF}" && git push origin "refs/tags/v${VERSION}"; then
         break
